2547147b26d8a13b8c99ea5ab2f996e550d2a022
star <startestyyds@gmail.com>
2026-01-09 09:49:23 +0800

    docs: dev
    配置文件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 
-import store from "./store";
-import router from "./router";
+import store from "@/store";
+import router from "@/router";
 
 import App from "./App.vue";
 
@@ -11,6 +11,8 @@ import "@/assets/css/tailwind.css";
 import "@/assets/css/index.scss";
 
 const app = createApp(App);
+document.title = import.meta.env.VITE_APP_TITLE || "默认标题";
+
 app.use(store);
 app.use(router);
 
